86bd1fd6f748dc2a8b293c2429b515f30fbdd4f5
Kevin <michaelkevinmendes@gmail.com>
2022-02-07 22:06:34 -0300

    Adicionando aparencia ao tabuleiro

--- src/eyesight/JBoard.java
+++ src/eyesight/JBoard.java
@@ -1,7 +1,12 @@
 package eyesight;
 
+
 import javax.swing.JPanel;
 import model.Board;
+import model.Piece;
+
+import java.awt.GridBagLayout;
+
 
 public class JBoard extends JPanel {
 
@@ -11,4 +16,26 @@ public class JBoard extends JPanel {
         this.tabuleiro = tabuleiro;
     }
     
+    public void desenhaTabuleiro() {
+        this.removeAll();
+        this.setLayout(new GridLayout(8, 8));
+        
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                JCell JCell;
+                Piece peca = this.tabuleiro.getPiece(i, j);
+                
+                if (peca == null) {
+                    JCell = new JCell(i, j); 
+                
+                } else {
+                    JCell = new JCell(new JPiece(peca));
+                }
+            }
+        }
+    }
+
+    private void setLayout(GridLayout gridLayout) {
+    }
 }
+
