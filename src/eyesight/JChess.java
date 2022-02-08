a75d84107bb5042ba8091f9d291a5223e7a81d52
Kevin <michaelkevinmendes@gmail.com>
2022-02-07 22:08:29 -0300

    Add executavel do tabuleiro

--- src/eyesight/JChess.java
+++ src/eyesight/JChess.java
@@ -2,6 +2,20 @@ package eyesight;
 
 import javax.swing.JFrame;
 
+import model.Board;
+
 public class JChess extends JFrame {
     
+    public JChess() {
+        setTitle("Jogo de xadrez");
+        this.add(new JBoard(new Board()));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JChess();
+    }
 }
